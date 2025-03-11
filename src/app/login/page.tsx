de165c19fa7579f8010b66f5ecd90b94568a0c99
JOSÉ BRUNO OLIVEIRA DA SILVA <jose-bruno_silva@estudante.sesisenai.org.br>
2025-03-10 21:52:08 -0300

    Semana 06

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+
+export default function Login() {
+    return (
+        <div>
+            <h1>login</h1>
+            <Link href="/">
+                <button>Home</button>
+            </Link>
+        </div>
+    )
+}
