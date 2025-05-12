18f6ebe9cbcaee16ffe53cc21ae8adf837601836
JOSÉ BRUNO OLIVEIRA DA SILVA <jose-bruno_silva@estudante.sesisenai.org.br>
2025-05-12 19:46:21 -0300

    12/05

--- back-end/src/controller/TaskController.ts
+++ back-end/src/controller/TaskController.ts
@@ -3,11 +3,11 @@ import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { taskService } from "../services/TaskService";
 
 export async function taskController(app: FastifyInstance) {
-    app.post("/task", async(request, reply) => {
+    app.post("/task", async (request, reply) => {
         const body = request.body as { text: string };
 
         try {
-            taskService.create(body.text);
+            await taskService.create(body.text);
             return reply.code(201).send();
         } catch (error: any) {
             return reply.code(409).send({ erro: error.message })
@@ -19,41 +19,24 @@ export async function taskController(app: FastifyInstance) {
         return reply.code(200).send(list);
     })
 
-    app.get("/task/:id", (request: FastifyRequest, reply: FastifyReply) => {
-       const { id } = request.params as { id: string };
-       const task = taskService.getById(id);
-       return task;
-    })
-
     app.patch("/task/:id/completed", async (request, reply) => {
         // CAPTURA INFORMAÇÃO
-        const { id } =  request.params as { id: string };
-        
+        const { id } = request.params as { id: string };
+
         try {
             // RAPASSA INFO RECEBIDA E RECEBE INFORMAÇÃO PROCESSADA
             const task = await taskService.updateCompleted(id);
             // RETORNA UMA RESPONSE PARA QUEM CHAMOU A ROTA
             return reply.code(200).send(task);
-        }catch(error: any) {
-            return reply.code(404).send({ error: error.message})
+        } catch (error: any) {
+            return reply.code(404).send({ error: error.message })
         }
     });
 
-    app.patch("/task/:id/text", (request, reply) => {
+    app.delete('/task/:id', async (request, reply) => {
         const { id } = request.params as { id: string };
-        const { text } = request.body as { text: string };
-
-        try {
-            const task = taskService.updateText(id, text);
-            return reply.code(200).send(task);
-        }catch(error: any) {
-            return reply.code(404).send({ error: error.message });
-        }
+        await taskService.deleteTask(id);
+        return reply.code(200).send();
     })
 
-    app.delete("/task/:id", async(request,replay)=>{
-        const {id} = request.params as {id: string}
-        await taskService.deleteTask(id)
-        return replay.code(200).send()
-    })
 }
